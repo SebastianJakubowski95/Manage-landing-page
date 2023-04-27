a41d20183314a37fbb05bcde508a39372d6a76c8
sebjak95@gmail.com <sebjak95@gmail.com>
2023-04-27 19:59:17 +0200

    add files

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ let isDown = false;
 let startX;
 let scrollLeft;
 
+function showModal() {
+  $("#burger").addClass("hide");
+  $("#close").removeClass("hide");
+  $("#modal").removeClass("hide");
+  $("body").addClass("prevent-scroll");
+}
+
+function hideModal() {
+  $("#burger").removeClass("hide");
+  $("#close").addClass("hide");
+  $("#modal").addClass("hide");
+  $("body").removeClass("prevent-scroll");
+}
+
+if (w <= 768) {
+  $("#header-cta").addClass("hide");
+  $("#navbar").addClass("hide");
+  $("#burger").removeClass("hide");
+}
+
+$("#burger").on("click", () => {
+  showModal();
+});
+
+$("#close").on("click", () => {
+  hideModal();
+});
+
 slider.addEventListener("mousedown", (e) => {
   isDown = true;
   slider.classList.add("active");
@@ -22,7 +50,13 @@ slider.addEventListener("mousemove", (e) => {
   if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 1; //scroll-fast
+  let walk;
+  if (w <= 768) {
+    walk = (x - startX) * 2; //scroll-fast
+  } else {
+    walk = (x - startX) * 1; //scroll-fast
+  }
+  // const walk = (x - startX) * 1; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
   console.log(walk);
 });
