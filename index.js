1095f4512a17c3c31546488864dbba90fb4b49c1
sebjak95@gmail.com <sebjak95@gmail.com>
2023-04-28 11:03:19 +0200

    add files

--- index.js
+++ index.js
@@ -4,33 +4,35 @@ let isDown = false;
 let startX;
 let scrollLeft;
 
-function showModal() {
-  $("#burger").addClass("hide");
-  $("#close").removeClass("hide");
-  $("#modal").removeClass("hide");
-  $("body").addClass("prevent-scroll");
-}
+// function showModal() {
+//   $("#burger").addClass("hide");
+//   $("#close").removeClass("hide");
+//   $("#modal").removeClass("hide");
+//   $("body").addClass("prevent-scroll");
+//   $("#navbar-modal").removeClass("hide");
+// }
 
-function hideModal() {
-  $("#burger").removeClass("hide");
-  $("#close").addClass("hide");
-  $("#modal").addClass("hide");
-  $("body").removeClass("prevent-scroll");
-}
+// function hideModal() {
+//   $("#burger").removeClass("hide");
+//   $("#close").addClass("hide");
+//   $("#modal").addClass("hide");
+//   $("body").removeClass("prevent-scroll");
+//   $("#navbar-modal").addClass("hide");
+// }
+// hideModal();
+// if (w <= 768) {
+//   $("#header-cta").addClass("hide");
+//   $("#navbar").addClass("hide");
+//   $("#burger").removeClass("hide");
+// }
 
-if (w <= 768) {
-  $("#header-cta").addClass("hide");
-  $("#navbar").addClass("hide");
-  $("#burger").removeClass("hide");
-}
+// $("#burger").on("click", () => {
+//   showModal();
+// });
 
-$("#burger").on("click", () => {
-  showModal();
-});
-
-$("#close").on("click", () => {
-  hideModal();
-});
+// $("#close").on("click", () => {
+//   hideModal();
+// });
 
 slider.addEventListener("mousedown", (e) => {
   isDown = true;
