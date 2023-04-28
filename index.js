f34c422416e2dfa60a9137298fd35c7a6d50db89
sebjak95@gmail.com <sebjak95@gmail.com>
2023-04-28 19:37:47 +0200

    add files

--- index.js
+++ index.js
@@ -4,36 +4,6 @@ let isDown = false;
 let startX;
 let scrollLeft;
 
-// function showModal() {
-//   $("#burger").addClass("hide");
-//   $("#close").removeClass("hide");
-//   $("#modal").removeClass("hide");
-//   $("body").addClass("prevent-scroll");
-//   $("#navbar-modal").removeClass("hide");
-// }
-
-// function hideModal() {
-//   $("#burger").removeClass("hide");
-//   $("#close").addClass("hide");
-//   $("#modal").addClass("hide");
-//   $("body").removeClass("prevent-scroll");
-//   $("#navbar-modal").addClass("hide");
-// }
-// hideModal();
-// if (w <= 768) {
-//   $("#header-cta").addClass("hide");
-//   $("#navbar").addClass("hide");
-//   $("#burger").removeClass("hide");
-// }
-
-// $("#burger").on("click", () => {
-//   showModal();
-// });
-
-// $("#close").on("click", () => {
-//   hideModal();
-// });
-
 slider.addEventListener("mousedown", (e) => {
   isDown = true;
   slider.classList.add("active");
@@ -54,11 +24,10 @@ slider.addEventListener("mousemove", (e) => {
   const x = e.pageX - slider.offsetLeft;
   let walk;
   if (w <= 768) {
-    walk = (x - startX) * 2; //scroll-fast
+    walk = (x - startX) * 2;
   } else {
-    walk = (x - startX) * 1; //scroll-fast
+    walk = (x - startX) * 1;
   }
-  // const walk = (x - startX) * 1; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
   console.log(walk);
 });
